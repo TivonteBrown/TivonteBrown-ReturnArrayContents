21ef0ac0a801b05f324a6e68af6c19fcfd65a42b
TivonteBrown <TivonteBrown@Gmail.com>
2023-02-04 23:20:17 +0000

    commit return array

--- src/main/java/ReturnArrayContents.java
+++ src/main/java/ReturnArrayContents.java
@@ -27,7 +27,15 @@ public class ReturnArrayContents {
      * @param nums the array to be manipulated.
      * @return a string representation of all values of nums, with the values separated by spaces.
      */
+        
     public String arrayToString(int[] nums){
-        return "";
+    String str = " ";
+         for(int i = 0; i < nums.length; i++){
+          String chInt = String.valueOf(nums[i]); 
+            str += " " + chInt;
+           
+
+         }
+    return str;
     }
 }
